dde4647f5d3ec78a9a8389d4b56a51225af57194
samar ben hadj abbes <samarabbes3@gmail.com>
2021-11-17 11:13:42 +0100

    second commit

--- app/src/main/java/com/samar/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/samar/affirmations/adapter/ItemAdapter.kt
@@ -4,17 +4,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.model.Affirmation
 import com.samar.affirmations.R
-import com.samar.affirmations.model.Affirmation
 
 class ItemAdapter(private val context: Context,
                   private val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
-    }
+        val imageView: ImageView = view.findViewById(R.id.item_image)    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -25,7 +26,8 @@ class ItemAdapter(private val context: Context,
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text =  context.resources.getString(item.stringResourceId)
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
